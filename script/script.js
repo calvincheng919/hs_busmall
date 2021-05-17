c13e6afb96479eb90e484406f50ca9ec45b2ac8b
Calvin Cheng <calvincheng919@gmail.com>
2021-05-17 08:55:09 -0700

    code.5.17.2021

--- script/script.js
+++ script/script.js
@@ -53,3 +53,32 @@ function runFunctionImage1() {
     console.log('I am in img1')
 }
 
+// We need some way of holding all the images in memory (or at least the locations of them)
+// We need an array of images... 
+// - array will have path to the image (url to image)
+// - array will also have total number of clicks (votes)
+// - array will also track total times an image is viewed 
+// - array will need the image HTMLid - example - bag, banana, boots, etc...
+
+// What kind of array will keep track of the above?
+
+// How do I create an array?
+
+
+// This is the mini-database to keep track of everything. 
+let PRODUCTS_ARRAY = [
+  { HTMLid : 'bag', imgURL: '../images/bag.jpg', totalViews: 0,totalVotes: 0},
+  { HTMLid : 'banana', imgURL: '../images/banana.jpg', totalViews: 0,totalVotes: 0},
+  { HTMLid : 'bathroom', imgURL: '../images/bathroom.jpg', totalViews: 0,totalVotes: 0},
+  { HTMLid : 'breakfast', imgURL: '../images/breakfast.jpg', totalViews: 0,totalVotes: 0},
+]
+
+// Consolidate all the response functions (callbacks) into one function
+function handleClick() {
+  // put all the callbacks into one place
+}
+
+// On startup - display 3 images from the array
+function renderImages() {
+  // this funtion will render 3 images from the array
+}
